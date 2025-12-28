ed425b0cb0f779c7e22b7a67a6e8b75e8730f3a3
Farheen1801 <waheedashafi12345@gmail.com>
2025-12-28 18:51:07 +0530

    Update selection_sort.c

--- selection_sort.c
+++ selection_sort.c
@@ -1,14 +1,39 @@
-#include<stdio.h>
-void selection_sort(int arr[],int n){
+ #include <stdio.h>
+                                          // uploaded
+int main() {
+    int a[50], n, i, j, min, temp;
 
-}
-int main(){
-int n,i;
-int arr[20];
-printf("enter number of elements");
-scanf("%d",&n);
-printf("enter elements");
-for(i=0;i<n;i++){
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
 
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Selection Sort logic
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+
+        for (j = i + 1; j < n; j++) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
+        }
+
+        // Swap
+        if (min != i) {
+            temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
+        }
+    }
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
 }
-}
+
