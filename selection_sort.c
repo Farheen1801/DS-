90598fa274ef03b891c9dba65ef39aae2c27c959
Farheen1801 <waheedashafi12345@gmail.com>
2025-12-28 13:04:14 +0530

    Add files via upload

---
+++ selection_sort.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void selection_sort(int arr[],int n){
+
+}
+int main(){
+int n,i;
+int arr[20];
+printf("enter number of elements");
+scanf("%d",&n);
+printf("enter elements");
+for(i=0;i<n;i++){
+
+}
+}
